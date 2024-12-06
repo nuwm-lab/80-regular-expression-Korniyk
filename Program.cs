818e6dcbb8c1a00d0b71a04234d8cc243e10fabf
Michael Korniichuk <misha17092006@gmail.com>
2024-12-06 23:16:24 +0200

    Added Lab8  in main

--- Program.cs
+++ Program.cs
@@ -1,18 +1,39 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace LabWork
+class Program
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    static List<string> FindAbbreviations(string text, string pattern)
     {
-        static void Main(string[] args)
+        MatchCollection matches = Regex.Matches(text, pattern);
+        return matches.Select(m => m.Value).ToList();
+    }
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        string text =
+            "У сучасному веб-розробці часто використовуються технології, такі як HTML, " +
+            "CSS, JavaScript, а також різноманітні фреймворки, як React, Angular, та бібліотеки, як jQuery. " +
+            "Багато програм для розробки також підтримують мови баз даних, такі як SQL, NoSQL, а також інші формати, наприклад, JSON, XML, YAML. " +
+            "У сучасному програмуванні часто застосовують такі інструменти, як Git, Docker, Kubernetes. " +
+            "Інші популярні технології включають C++, C#, Swift, Rust.\r\n";
+
+        string pattern = @"\b[A-Z0-9]+[#\+]*\b";
+
+        var abbreviations = FindAbbreviations(text, pattern);
+
+        if (abbreviations.Any())
         {
-            
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Знайдені абревіатури:");
+            abbreviations.ForEach(Console.WriteLine);
+        }
+        else
+        {
+            Console.WriteLine("Абревіатур не знайдено.");
         }
     }
 }
